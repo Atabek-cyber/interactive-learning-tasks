f243a0a0d86695fe3376b1dc302c78e7a605dedc
Atabek-cyber <atabekbekenov@yahoo.com>
2021-08-03 21:05:15 +0000

    added

---
+++ sec_group/sec_group.tf
@@ -0,0 +1,46 @@
+resource "aws_vpc" "external_by_terraform" {
+  cidr_block = "10.1.0.0/16"
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+}
+
+
+  ingress {
+   description = "TLS"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["50.194.68.230/32"]
+  }
+   ingress {
+    description = "TLS"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["50.194.68.230/32"]
+
+  }
+   ingress {
+    description = "TLS"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    
+  }
+   ingress {
+    description = "TLS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
